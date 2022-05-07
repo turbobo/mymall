6fb4629d1c8a8a14002b3dfae6a51aa7fed8585d
turbobo <735567435@qq.com>
2022-05-07 16:36:43 +0800

    创建各微服务模块

---
+++ mymall-member/src/test/java/com/turbo/mymall/member/MymallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbo.mymall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mymall-product/src/test/java/com/turbo/mymall/product/MymallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbo.mymall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
